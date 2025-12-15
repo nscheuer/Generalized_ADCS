839bd076ecba47834f687d89b5869218ec7767a4
Niclas Scheuer <niclas_scheuer@web.de>
2025-12-15 01:04:39 -0500

    controller architecture updated with reaction wheels

--- debug/debug_mtq_w_rw.py
+++ debug/debug_mtq_w_rw.py
@@ -77,7 +77,7 @@ def test_mtq_w_rw_align_to_eci(verbose: bool = False, tf: float = 1000, dt: floa
     time_hist = np.nan*np.zeros(N)
     state_hist = np.nan*np.zeros((N, 10))
     os_hist: List[Orbital_State] = list()
-    sensor_hist: np.ndarray = np.nan*np.zeros((N, 3))
+    sensor_hist: np.ndarray = np.nan*np.zeros((N, len(real_sat.sensors + real_sat.rw_actuators)))
     u_hist = np.nan*np.zeros((N, len(acts)))
     boresight_hist = np.nan*np.zeros((N, 3))
 
