e7b94ac8cc531c166997a0bf55676759cbfc42a0
Niclas Scheuer <niclas_scheuer@web.de>
2026-01-07 21:19:00 -0500

    remove references to pysat

--- ADCS/controller/__init__.py
+++ ADCS/controller/__init__.py
@@ -1,8 +1,7 @@
 from .controller import Controller
 from .bdot import BDot
 from .mtq_w_rw import MTQ_w_RW
-from .plan_and_track_lqr import Plan_and_Track_LQR
 from .combined_mtq import Combined_MTQ
 from .mtq_w_1rw import MTQ_w_1RW
 
-__all__ = ["Controller", "BDot", "MTQ_w_RW", "Plan_and_Track_LQR", "Combined_MTQ", "MTQ_w_1RW"]
+__all__ = ["Controller", "BDot", "MTQ_w_RW", "Combined_MTQ", "MTQ_w_1RW"]
