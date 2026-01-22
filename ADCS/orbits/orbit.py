Fix IGRF radius units and verbose_level initialization
Bug fixes:
- IGRF: geocentric[0] is already in km (same as R input), so don't divide by 1000
  This was incorrectly introduced in commit 0e0a571 with wrong comment
- verbose_level: Initialize to 0 in OldPlanner constructor to avoid undefined behavior

ADCS/orbits/orbit.py
@@ -220,8 +220,8 @@ class Orbit:
         geos = np.vstack([self.states[j].geocentric for j in self.times])
         dts = [self.states[j].datetime for j in self.times]
         # IGRF expects radius in km, theta and phi in degrees
-        # geos[:,0] is radius in meters, so divide by 1000
-        b_r, b_th, b_ph = ppigrf.igrf_gc(geos[:,0]/1000.0, geos[:,1]*180.0/np.pi, geos[:,2]*180.0/np.pi, dts)
+        # geos[:,0] is already in km (same units as R input to Orbital_State)
+        b_r, b_th, b_ph = ppigrf.igrf_gc(geos[:,0], geos[:,1]*180.0/np.pi, geos[:,2]*180.0/np.pi, dts)
         b_r = np.diagonal(b_r)
         b_th = np.diagonal(b_th)
         b_ph = np.diagonal(b_ph)

ADCS/orbits/orbital_state.py
@@ -401,13 +401,14 @@ class Orbital_State:
         return vec @ self.ECI2ENUmat
 
     def get_b_eci(self) -> np.ndarray:
-        r_m = self.geocentric[0]
+        r_km = self.geocentric[0]  # Already in km (same units as R input)
         theta_rad = self.geocentric[1]
         phi_rad = self.geocentric[2]
 
         # IGRF expects radius in km, theta and phi in degrees
+        # geocentric[0] is already in km since R is passed in km
         b_r, b_th, b_ph = ppigrf.igrf_gc(
-            r_m / 1000.0,  # Convert meters to km
+            r_km,
             theta_rad * 180.0 / np.pi,
             phi_rad * 180.0 / np.pi,
             self.datetime,